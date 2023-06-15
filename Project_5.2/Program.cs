497576a85676634dbb16f3d39ba689edb63d6205
Yanberu <115783188+Yanberu@users.noreply.github.com>
2023-06-15 20:09:32 +0300

    Two tasks completed

--- Project_5.2/Program.cs
+++ Project_5.2/Program.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace Project_5
+namespace Project_5._2
 {
-   
     internal class Program
     {
         static string[] TextSplit(string text)
@@ -17,24 +15,34 @@ namespace Project_5
             return textSplit;
         }
 
-        static void OutputWords(string[] result) 
+
+        static string ReverseString(string text)
         {
+            string[] result = TextSplit(text);
             string resultString = "";
-            for (int i = 0; i <= result.Length - 1; i++)
+            for (int i = result.Length - 1; i >= 0 ; i--)
             {
                 resultString += result[i] + " ";
             }
+            return resultString;
+        }
+
+        static void OutputWords(string resultString)
+        {
+            
             Console.WriteLine(resultString);
         }
 
         static void Main(string[] args)
         {
-
             string text = Console.ReadLine();
-            string[] result = TextSplit(text);
-            OutputWords(result);
+
+            string resultString = ReverseString(text);
+            OutputWords(resultString);
             Console.ReadLine();
         }
     }
 }
+
+
 
