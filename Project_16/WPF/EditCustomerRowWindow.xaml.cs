e8f3eb2c6df19c8242c892a3fd7c70e219218990
Yanberu <115783188+Yanberu@users.noreply.github.com>
2023-07-31 19:19:32 +0300

    Project_16 added

---
+++ Project_16/WPF/EditCustomerRowWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Diagnostics;
+
+namespace Project_16.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для EditRowWindow.xaml
+    /// </summary>
+    public partial class EditCustomerRowWindow : Window
+    {
+        private DataRow dataRow;
+        private bool flag;
+        private EditCustomerRowWindow()
+        {
+            InitializeComponent();
+        }
+
+        public EditCustomerRowWindow(DataRowView dataRowView) : this()
+        {
+            
+            DataContext = dataRowView;
+            flag = false;
+        }
+
+        public EditCustomerRowWindow(DataRow dataRow) : this()
+        {
+            
+            this.dataRow = dataRow;
+            flag = true;
+            //DataContext = dataRow.Table;
+        }
+
+        private void btOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+            {
+                try
+                {
+                    dataRow["LastName"] = tbLastName.Text;
+                    dataRow["FirstName"] = tbFirstName.Text;
+                    dataRow["MiddleName"] = tbMiddleName.Text;
+                    dataRow["PhoneNumber"] = tbPhoneNumber.Text;
+                    dataRow["Email"] = tbEmail.Text;
+                } catch (Exception ex)
+                {
+                    Debug.WriteLine($"ERROR btOk_Click in EditCustomerRowWindow {ex.Message}");
+                    this.DialogResult = false;
+                    return;
+                }
+                
+            }              
+
+
+            this.DialogResult = true;
+        }
+    }
+}
---
+++ Project_16/WPF/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Project_16.WPF;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Project_16
+{
+    public partial class MainWindow : Window
+    {
+        public Repository rep;
+        public MainWindow()
+        {
+            InitializeComponent();
+            rep = new Repository(@"C:\Users\Ivan\Documents\GitHub\Projects\Project_16\DataBases", "123");
+            dataGridCustomers.ItemsSource = rep.CustomersTable.DefaultView;
+            dataGridOrders.ItemsSource = rep.OrdersTable.DefaultView;
+            btEmailFilter.Click += (s, e) => {rep.SetFindForEmailAccessSelectCommand(GetSelectedString(dataGridCustomers.SelectedItem,"Email")); };
+            btNoFilter.Click += (s, e) => { rep.SetTablesDefault(); };
+        }
+        private string GetSelectedString(object selectedItem, string findStringToCell)
+        {
+            if (selectedItem == null) return null;
+            if (selectedItem.GetType() != typeof(DataRowView)) return null;
+            DataRowView drw = selectedItem as DataRowView;
+            return drw[findStringToCell].ToString();
+        }
+
+        #region MENU
+
+        private void CustomersEdit_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView dataRowView = (DataRowView)dataGridCustomers.SelectedItem;
+            if (dataRowView == null) return;
+            EditCustomerRowWindow window = new EditCustomerRowWindow(dataRowView);
+            if (window.ShowDialog() == true) rep.CustomersAdapterUpdate();
+        }
+        private void CustomersAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //rep.dataRowView = (DataRowView)dataGridCustomers.Items[dataGridCustomers.Items.Count-1];
+            DataRow r = rep.CustomersTable.NewRow();
+            
+            EditCustomerRowWindow window = new EditCustomerRowWindow(r);
+            if (window.ShowDialog() == true)
+            {
+                rep.CustomersTable.Rows.Add(r);
+                rep.CustomersAdapterUpdate();
+            }
+        }
+        private void CustomersDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView dataRowView = (DataRowView)dataGridCustomers.SelectedItem;
+            dataRowView.Delete();
+            rep.CustomersAdapterUpdate();
+        }
+
+        private void OrdersEdit_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView dataRowView = (DataRowView)dataGridOrders.SelectedItem;
+            if (dataRowView == null) return;
+            EditOrderRowWindow window = new EditOrderRowWindow(dataRowView);
+            if (window.ShowDialog() == true) rep.OrdersAdapterUpdate();
+        }
+
+        private void OrdersAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow r = rep.OrdersTable.NewRow();
+
+            EditOrderRowWindow window = new EditOrderRowWindow(r);
+            if (window.ShowDialog() == true)
+            {
+                rep.OrdersTable.Rows.Add(r);
+                rep.OrdersAdapterUpdate();
+            }
+        }
+
+        private void OrdersDelete_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView dataRowView = (DataRowView)dataGridOrders.SelectedItem;
+            dataRowView.Delete();
+            rep.OrdersAdapterUpdate();
+        }
+        #endregion
+
+
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
